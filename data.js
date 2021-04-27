35f9bcc249db5347f70e55cbacdbe0b72a2c468c
BenHurJF <beiujeffer@hotmail.com>
2021-04-27 04:04:54 -0300

    Enviando para GitHub

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: 'vrLVjHxiIiA',
+        title: 'NUNCA DEIXE TE SABOTAREM!',
+        duration: '13 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'm6KeVshClzw',
+        title: 'VOCÊ TAMBÉM PODE VENCER, NÃO DESISTA!',
+        duration: '1:06:00 min',
+        price: 'Free'
+    },
+    {
+        id: '17W8hE4Lg0I',
+        title: 'A CAMINHADA É DURA, MAS VALE A PENA',
+        duration: '42:22 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'AFsSEgPoMWA',
+        title: 'NÃO CAIA EM ILUSÕES',
+        duration: '21:54 min',
+        price: 'Free'
+    },
+    {
+        id: 'ysKE5ETAfUA',
+        title: 'CUIDE DAQUILO QUE PODE TE DERRUBAR',
+        duration: '11:27 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'ECw9x_adT3Q',
+        title: 'TENHA UMA ROTINA DE EVOLUÇÃO',
+        duration: '13 min',
+        price: 'Free'
+    }
+]
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const videos = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get("/", function (req, res) {
+    const about = {
+        img: "https://img.youtube.com/vi/AFsSEgPoMWA/maxresdefault.jpg",
+        name: "Luan Kilzes motivation",
+        descH3: "Live intensely every day",
+        descP: "Viva intensamente todos os dias!",
+        links: [
+            { name: "Facebook", url: "https://www.facebook.com/beiu.jeffer/" },
+            {    name: "Instagram", url: "https://instagram.com/beiujeffer/" }
+        ]
+    }
+
+    return res.render("about", { about })
+})
+
+server.get("/aulas", function (req, res) {
+
+    return res.render("aulas", { items: videos })
+})
+
+server.get("/video", function (req, res) {
+    const id = req.query.id
+
+    const video = videos.find(function (video) {
+        return video.id == id
+
+   /*  OU ->   if (video.id == id) {
+            return true
+        } */
+    }) 
+
+    if(!video) {
+       return res.send("Video not found")
+    }
+
+       return res.render("video", { item: video } )
+})
+
+server.listen(5000, function () {
+    console.log("servidor rodando")
+})
